2440e24b35babd3adb0475f9f2043bcb6cad6a1f
Oleg Valter <o.a.philippov@gmail.com>
2021-07-23 00:43:15 +0300

    rebuilt dist

--- dist/modern/index.min.js
+++ dist/modern/index.min.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @author          Oleg Valter
-// @description     Fix chat search input without having to wait 6 to 8 weeks
+// @description     Fix chat Stack Exchange community search without having to wait 6 to 8 weeks
 // @grant           none
 // @homepage        https://github.com/userscripters/chat-search-fixup#readme
 // @match           https://chat.stackexchange.com
 // @match           https://chat.stackoverflow.com
-// @name            chat-search-fixup
+// @name            chat--search-fixup
 // @namespace       userscripters
 // @source          git+https://github.com/userscripters/chat-search-fixup.git
 // @supportURL      https://github.com/userscripters/chat-search-fixup/issues
